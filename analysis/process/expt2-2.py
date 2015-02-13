3b43b0d43c1c2e2ec7448ef0b64f9bb41b2112be
andrewtpierce <andtpierce@gmail.com>
2015-02-12 17:29:22 -0800

    added more data

--- analysis/process/expt2-2.py
+++ analysis/process/expt2-2.py
@@ -3,18 +3,37 @@ import refltool as rt
 from os import chdir 
 from pylab import *
 
+
+############
+##import current data
+
 #this one is half-sapph-no-ito
-
 chdir("../data/expt2")
+data_new=rt.reflm_std("mirror_VNATrc.001","mirror_VNATrc.002","half-sapph-no-ito_VNATrc.001","half-sapph-no-ito_VNATrc.002")
 
-data=rt.reflm_std("mirror_VNATrc.001","mirror_VNATrc.002","half-sapph-no-ito_VNATrc.001","half-sapph-no-ito_VNATrc.002")
+###########
+##import old data
+
+chdir("../sapph-old")
+data_old=rt.reflm_std("mirror_VNATrc.001","mirror_VNATrc.002","sapphire_VNATrc.001","sapphire_VNATrc.002")
+
+
 
 ########
 #inputs
 #######
 
-freq=data[:,0]
+freq=data_new[:,0]
 
+plot(freq,data_old[:,2:3])
+plot(freq,data_new[:,2:3])
+#plot(freq,abs(rt.f2c(data_new[:,2:4]))**2)
+#plot(freq,abs(rt.f2c(data_old[:,2:4]))**2)
+ylim(-1.5,1.5)
+show()
+
+
+'''
 #incident e-field phase at surface
 ph_i=0.
 #..gives incident e-field, normalized to E_max = 1
@@ -28,37 +47,9 @@ e_refl= e_inc*rt.f2c(data[:,2:4])[:,0]
 
 
 e_tot=e_inc+e_refl
-
-'''
-plot(freq,abs(e_tot)**2)
-ylim(-1,2)
-show()
-'''
-
-#plot(freq,e_tot.real)
-########################################################################################################################
-
-#incident e-field phase at surface
-ph_i=0.
-#..gives incident e-field, normalized to E_max = 1
-e_inc=np.exp(1j*ph_i)
-
-
-#reflected e-field
-e_refl= e_inc*rt.f2c(data[:,2:4])[:,0]
-
-#total field at surface is real part of ( incident e-field at surface plus reflected e-field at surface)
-
-
-e_tot=e_inc+e_refl
-
-'''
-plot(freq,abs(e_tot)**2)
-ylim(-1,2)
-show()
-'''
 
 plot(freq, e_tot.real)
 plot(freq, e_refl.real)
 ylim(-1.5,1.5)
 show()
+'''
