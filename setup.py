69b89fdf263cab7d61a639afb323b7c33f96b2d6
andrewtpierce <andtpierce@gmail.com>
2015-04-24 17:51:31 -0700

    added setup.py; working on transfer matrix
    Added setup.py file for setuptools. Working on script in
    scripts/epr-cavity which includes transfer matrix analysis for
    multi-slab geometry.

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 10 22:49:13 2015
-
-@author: dreadnought
-
-"Brevity required, prurience preferred"
-"""
-
 from setuptools import setup
 
-setup(name='hsganalysis',
-      #version='1.0',
-      #url = 'http://github.com/braydenware/PEPS',
-      #author = 'Brayden Ware',
-      #author_email = 'brayden@physics.ucsb.edu',
-      #license = 'MIT',
+setup(name='refltool',
       packages = [],
       install_requires = [],
-      #entry_points={'console_scripts': ['runPEPS = PEPS.command_line:main']},
       include_package_data = True,
       zip_safe = False)
